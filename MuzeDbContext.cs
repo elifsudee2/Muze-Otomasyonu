9c22d3624dec632e5489b32f4ea3f94301a61cf5
elifsudee2 <guneselifsudee@gmail.com>
2026-06-04 00:26:49 +0300

    MuzeDbContext.cs düzeltildi

--- MuzeDbContext.cs
+++ MuzeDbContext.cs
@@ -10,15 +10,13 @@ namespace MuzeOtomasyonu2
 {
     internal class MuzeDbContext : DbContext
     {
-        // 1. ADIM: Constructor (Yapıcı Metot)
-        // App.config dosyasında verdiğimiz "MuzeDbContext" ismini buraya yazarak bağlantıyı kuruyoruz.
+        
         public MuzeDbContext() : base("name=MuzeDbContext")
         {
 
         }
 
-        // 2. ADIM: DbSet Tanımlamaları
-        // Model sınıflarımızı veritabanı tabloları olarak tanıtıyoruz.
+       
         public DbSet<Eserler> Eserler { get; set; }     
         public virtual DbSet<Kategori> Kategoriler { get; set; }
         public virtual DbSet<Sanatcılar> Sanatcılar { get; set; }
